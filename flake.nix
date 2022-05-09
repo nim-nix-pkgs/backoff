5f0a21390d33375823cab6d6c9407abd6aa03477
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:23 -0300

    chore: re index backoff

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."backoff-master".dir   = "master";
-  inputs."backoff-master".owner = "nim-nix-pkgs";
-  inputs."backoff-master".ref   = "master";
-  inputs."backoff-master".repo  = "backoff";
-  inputs."backoff-master".type  = "github";
-  inputs."backoff-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."backoff-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."backoff-v0_1".dir   = "v0_1";
-  inputs."backoff-v0_1".owner = "nim-nix-pkgs";
-  inputs."backoff-v0_1".ref   = "master";
-  inputs."backoff-v0_1".repo  = "backoff";
-  inputs."backoff-v0_1".type  = "github";
-  inputs."backoff-v0_1".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."backoff-v0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
